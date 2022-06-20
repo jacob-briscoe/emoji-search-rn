427a8830c4faedb7ee9941ef81e02c7b87658da0
Jacob Briscoe <41913083+jacob-briscoe@users.noreply.github.com>
2022-06-20 08:41:29 -0400

    Added react components to convert, filterEmojis, and the emoji list to search.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Emoji Search!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});

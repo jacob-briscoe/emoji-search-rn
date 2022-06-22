847728889a12e2877df7c50ced5b2b9df350565e
Jacob Briscoe <41913083+jacob-briscoe@users.noreply.github.com>
2022-06-21 21:19:52 -0400

    Updated default max search results

--- src/filterEmoji.js
+++ src/filterEmoji.js
@@ -1,6 +1,6 @@
 import emojiList from './emojiList.json';
 
-export default function filterEmoji(searchText, maxResults) {
+export default function filterEmoji(searchText, maxResults = 20) {
   return emojiList
     .filter(emoji => {
       if (emoji.title.toLowerCase().includes(searchText.toLowerCase())) {
